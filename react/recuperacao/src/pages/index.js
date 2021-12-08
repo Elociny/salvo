370402467c237e2dd066d068193222b57f25acaf
Nicole <nicole.lins04@gmail.com>
2021-12-08 19:04:05 -0300

    os humilhados foram exaltados

--- react/recuperacao/src/pages/index.js
+++ react/recuperacao/src/pages/index.js
@@ -1,9 +1,29 @@
 import { Div } from './styled';
 
-
+import { useEffect, useState } from 'react';
+import Api from '../services/api';
+const api = new Api();
 
 export default function Home() {
- 
+    const [nomes, setNomes] = useState([]);
+    const [nome, setNome] = useState('');
+
+    async function listando(){
+        let f = await api.listarNomes();
+        console.log(f)
+        setNomes(f);
+    }
+
+    async function inserindo() {
+        let f = await api.inserirNomes(nome);
+        alert('inseriu')
+
+        listando();
+    }
+
+    useEffect(()=> {
+        listando();
+    }, [])
 
     return (
         <Div>
@@ -18,11 +38,11 @@ export default function Home() {
                     </div>
 
                     <div className="input">
-                        <input type="text" placeholder="" ></input>
+                        <input type="text" value={nome} onChange={e => setNome(e.target.value)}></input>
                     </div>
 
                     <div className="botao">
-                        <button>OK</button>
+                        <button onClick={inserindo}>OK</button>
                     </div>
                 </div>
 
@@ -44,12 +64,15 @@ export default function Home() {
                                 <th> Nome </th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr >
-                                <td>1</td>
-                                <td>Nicole</td>
-                            </tr>
-                        </tbody> 
+
+                        {nomes.map(item =>
+                            <tbody>
+                                <tr >
+                                    <td> {item.id} </td>
+                                    <td>{item.id_nome}</td>
+                                </tr>
+                            </tbody> 
+                        )}
                     </table>
                 </div>
             </div>
